e53f2552371d49661138080eae3ff08909a8a026
HisamNasir <85251630+HisamNasir@users.noreply.github.com>
2024-01-03 15:07:10 +0500

    Update BottomText.tsx

--- app/ui/BottomText.tsx
+++ app/ui/BottomText.tsx
@@ -3,10 +3,10 @@ import { FaLock } from 'react-icons/fa'
 
 export default function BottomText() {
   return (
-    <div className=' text-[6px] sm:text-[7px] xl:text-[8px] 2xl:text-[10px]'>
-    <div className="absolute bottom-0 left-0 py-2 px-4 lg:p-6">
-      <div className=" flex gap-2 items-end">
-        <div className=' max-sm:flex'>
+    <div className=' text-[6px]  xl:text-[8px] 2xl:text-[10px]'>
+    <div className="absolute max-md:w-full bottom-0 left-0 p-2 lg:p-4">
+      <div className=" max-sm:flex max-sm:flex-col w-full max-sm:text-center justify-center">
+        <div className=' max-sm:flex max-sm:flex-col max-sm:text-center w-full justify-center'>
           <p>zitransfer 2023</p>
 
           <p>all right reserved</p>
@@ -15,9 +15,9 @@ export default function BottomText() {
       </div>
     </div>
 
-    <div  className="absolute bottom-5 sm:bottom-8 lg:bottom-0 right-0 py-2 px-4 lg:p-6">
+    <div  className="absolute bottom-8 md:bottom-0 right-0 p-2 lg:p-4">
       <div className="md:flex gap-2 items-end">
-        <p className="flex  gap-2 items-center max-md:text-left ">
+        <p className="flex  gap-2 items-center text-center sm:text-left ">
           <span>
             <FaLock />
           </span>
