d530c7447e352aedd61feeee8248260f0c57898f
HisamNasir <85251630+HisamNasir@users.noreply.github.com>
2024-01-05 21:57:40 +0500

    Update LeftSideCard.tsx

--- app/ui/LeftSideCard.tsx
+++ app/ui/LeftSideCard.tsx
@@ -122,8 +122,8 @@ const LeftSideCard = () => {
 
       <div id="second-card"></div>
     </div>
-    <div className="absolute rotate-90 translate-x-16 -right-7 font-normal lg:-right-[44px] top-28">
-      <p className=" text-[6px] lg:text-[7px] w-full">
+    <div className="absolute rotate-90 translate-x-16 -right-7 font-normal lg:-right-[80px] top-28 lg:top-[144px]">
+      <p className=" text-[6px] lg:text-[10px] w-full">
         advanced encryption standard (aes) 256-bit
       </p>
     </div>
