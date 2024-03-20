edea945ee1031e81eac2ad1df9f840d273b58d8d
HisamNasir <85251630+HisamNasir@users.noreply.github.com>
2024-03-20 12:57:03 +0500

    done

--- components/ImageWallpaper.jsx
+++ components/ImageWallpaper.jsx
@@ -7,34 +7,67 @@ const ImageWallpaper = () => {
   const dispatch = useDispatch();
   const currentImageIndex = useSelector((state) => state.background.currentImageIndex);
   const [videoFinished, setVideoFinished] = useState(false);
+  const [colorIndex, setColorIndex] = useState(0);
+
+  const colors = ['#FF0000', '#00FF00', '#0000FF', '#FFFF00', '#FF00FF', '#00FFFF', '#000000'];
 
   useEffect(() => {
     // Play video initially
     const video = document.getElementById('background-video');
 
-    video.addEventListener('ended', () => {
+    const handleVideoEnd = () => {
       setVideoFinished(true);
-      dispatch(setDominantColor('#000'));
-    });
+      dispatch(setDominantColor('#000')); // Set dominant color to black when video finishes
+    };
+
+    video.addEventListener('ended', handleVideoEnd);
 
     return () => {
-      video.removeEventListener('ended', () => {
-        setVideoFinished(true);
-        dispatch(setDominantColor('#000')); 
-      });
+      video.removeEventListener('ended', handleVideoEnd);
     };
   }, [dispatch]);
 
   useEffect(() => {
     if (videoFinished) {
       const intervalId = setInterval(() => {
-        dispatch(incrementImageIndex());
-        dispatch(extractDominantColor(`/media/images/${currentImageIndex + 2}.png`));
-      }, 5000); // Change image every 5 seconds
+        if (currentImageIndex === 59) {
+          setColorIndex((prevIndex) => (prevIndex + 1) % 7);
+        } else {
+          dispatch(incrementImageIndex());
+          dispatch(extractDominantColor(`/media/images/${currentImageIndex + 2}.png`));
+        }
+      }, 500); // Change image every 5 seconds
 
       return () => clearInterval(intervalId);
     }
   }, [dispatch, currentImageIndex, videoFinished]);
+
+
+  useEffect(() => {
+    if (videoFinished) {
+      const intervalId = setInterval(() => {
+        // Increment colorIndex to display the next color
+        setColorIndex((prevIndex) => (prevIndex + 1) % colors.length);
+
+        // If all colors have been displayed, reset colorIndex and increment imageIndex
+        if (colorIndex === colors.length - 1) {
+          dispatch(incrementImageIndex());
+        }
+
+        // If colorIndex is less than the length of colors array, dispatch the color
+        if (colorIndex < colors.length) {
+          dispatch(setDominantColor(colors[colorIndex]));
+        }
+
+        // If colorIndex reaches the end of the colors array, reset it
+        if (colorIndex === colors.length - 1) {
+          setColorIndex(0);
+        }
+      }, 5000); // Change color every 5 seconds
+
+      return () => clearInterval(intervalId);
+    }
+  }, [dispatch, colorIndex, videoFinished, colors]);
 
   return (
     <div className="image-wallpaper h-screen overflow-hidden absolute -z-50">
@@ -44,16 +77,22 @@ const ImageWallpaper = () => {
           Your browser does not support the video tag.
         </video>
       ) : (
-        <Image
-          src={`/media/images/${currentImageIndex + 1}.png`}
-          alt="Wallpaper"
-          width={1920}
-          height={1080}
-          className="w-screen h min-h-screen object-cover"
-          onLoad={() => {
-            dispatch(extractDominantColor());
-          }}
-        />
+        <>
+          {currentImageIndex !== 59 ? (
+            <Image
+              src={`/media/images/${currentImageIndex + 1}.png`}
+              alt="Wallpaper"
+              width={1920}
+              height={1080}
+              className="w-screen h min-h-screen object-cover"
+              onLoad={() => {
+                dispatch(extractDominantColor());
+              }}
+            />
+          ) : (
+            <div className="w-screen h-screen" style={{ backgroundColor: colors[colorIndex] }}></div>
+          )}
+        </>
       )}
     </div>
   );
